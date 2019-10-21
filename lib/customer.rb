2f7eb17b18dcd36b816699bad0a873c61f550286
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-21 03:41:56 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,47 @@
 class Customer
+  attr_accessor :name, :age 
   
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+    
+  def waiters
+    meals.map do |meal|
+      meal.waiter 
+    end
 end
+
+alex = Customer.new("Alex", 30)
+rahcel = Customer.new("Rachel", 27)
+dan = Waiter.new("Dan", 3)
+
+rachel.new_meal(dan, 50, 10)
+alex.new_meal(dan, 30, 5)
+
+rachel.meals
+rachel.meals.length 
+Meal.all.length
+terrance = Customer.new("Terrance", 27)
+jason = Waiter.new("Jason", 4)
+andrew = Waiter.new("Andrew", 7)
+yomi = Waiter.new("Yomi", 10)
+ 
+terrance.new_meal(jason, 50, 6)
+terrance.new_meal(andrew, 60, 8)
+terrance.new_meal(yomi, 30, 4)
+ 
+terrance.waiters
+terrance.waiters.last.name
